df501f53ad608272f5d3d1fa2bad687782bc88a4
pujisaadah <pujisaa214@gmail.com>
2023-12-23 11:19:35 +0700

    meet 12

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://da39-2404-8000-1028-10fb-71ca-1f9c-d828-6eb1.ngrok-free.app';
+  static const baseUrl = 'https://806e-2404-8000-1028-10fb-3192-ca55-6691-a46d.ngrok-free.app';
 }
